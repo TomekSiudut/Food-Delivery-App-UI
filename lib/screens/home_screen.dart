f4fb865b432aad59d0eca589b3314248002358ff
Tomasz Siudut <TomekSiudut@users.noreply.github.com>
2021-01-02 18:27:55 +0100

    cart screen completed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:flutter_food_delivery_ui/models/restaurant.dart';
+import 'package:flutter_food_delivery_ui/screens/cart_screen.dart';
 import 'package:flutter_food_delivery_ui/screens/restaurant_screen.dart';
 import 'package:flutter_food_delivery_ui/widgets/rating_stars.dart';
 import 'package:flutter_food_delivery_ui/widgets/recent_orders.dart';
@@ -82,7 +83,7 @@ class _HomeScreenState extends State<HomeScreen> {
           title: Text("Food Delivery"),
           actions: [
             FlatButton(
-                onPressed: () {},
+                onPressed: () => Navigator.push(context , MaterialPageRoute(builder:(_) => CartScreen())),
                 child: Text("Cart (${currentUser.cart.length})", style: TextStyle(color: Colors.white, fontSize: 15.0)))
           ],
           centerTitle: true,
